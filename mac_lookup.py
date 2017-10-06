da8cd3572bf7063a1e17208368f4d6e23b8f63e9
Kees de Jong <keesdejong+bugs@gmail.com>
2017-10-06 10:12:51 +0200

    time to add method to write new csv

--- mac_lookup.py
+++ mac_lookup.py
@@ -2,7 +2,7 @@
 
 from netaddr import EUI, mac_unix
 from sys import argv, exit
-import csv, re
+import csv, re, urllib2, json
 
 
 class MacLookup(object):
@@ -19,19 +19,21 @@ class MacLookup(object):
                 for row in reader:
                     mac = row[0]
                     if self.r.match(mac):
-                        if mac in self.vendor_details.keys():
-                            vendor_detail = self.get_vendor_details(mac)
-                            mac = vendor_detail[0]
-                            mac_info = vendor_detail[1:]
-                            vendor_details[mac].append(mac_info)
-                        else:
-                            vendor_detail = self.get_vendor_details(mac)
-                            mac = vendor_detail[0]
-                            mac_info = vendor_detail[1:]
-                            self.vendor_details[mac]=mac_info
+                        #if mac in self.vendor_details.keys():
+                        #    vendor_detail = self.get_vendor_details(mac)
+                        #    mac_dialect = vendor_detail[0]
+                        #    company = vendor_detail[1]
+                        #    mac_prefix = vendor_detail[2]
+                        #    self.vendor_details[mac].extend([(mac_dialect, company, mac_prefix)])
+                        #else:
+                        vendor_detail = self.get_vendor_details(mac)
+                        mac_dialect = vendor_detail[0]
+                        company = vendor_detail[1]
+                        mac_prefix = vendor_detail[2]
+                        self.vendor_details[mac]=[(mac_dialect, company, mac_prefix)]
                     else:
-                        print("%s is not a MAC" % (mac))
-                        continue
+                        mac = row[0]
+                        self.vendor_details[mac]='Invalid MAC'
             f.close()
             self.get_output()
         except IOError as e:
@@ -43,17 +45,31 @@ class MacLookup(object):
         converted_mac.dialect = mac_unix
         try:
             oui = converted_mac.oui
-        except Exception as e:
-            return(e)
-        return(str(converted_mac), oui.registration().org, oui.registration().oui)
+        except:
+            company, mac_prefix = self.get_vendor_details_online(converted_mac)
+            mac_prefix = mac_prefix.replace(':', '-')
+            return(str(converted_mac), str(company), str(mac_prefix))
+        return(str(converted_mac), str(oui.registration().org), str(oui.registration().oui))
 
     def get_vendor_details_online(self, mac):
-        # https://macvendors.co/api/C0:EE:FB:04:6B:91/json
-        print 'hoi'
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+        response = opener.open("https://macvendors.co/api/%s/json" % (mac))
+        vendor_detail = json.loads(response.read())
+        if 'result' in vendor_detail.keys():
+            try:
+                company = vendor_detail['result']['company']
+                mac_prefix = vendor_detail['result']['mac_prefix']
+                return(company, mac_prefix)
+            except KeyError:
+                return('N/A', 'N/A')
 
     def get_output(self):
         for key, value in self.vendor_details.items():
             print("%s: %s" % (key, value))
+
+    def write_csv(self):
+        print('write')
 
 
 def main():
