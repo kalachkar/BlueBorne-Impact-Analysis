ea31e6a20c32127d3f96811173ac7a0054c2eeb5
Kees de Jong <keesdejong+bugs@gmail.com>
2017-10-10 15:54:53 +0200

    FIXME: update columns without replacing the whole row

--- mac_lookup.py
+++ mac_lookup.py
@@ -1,80 +1,125 @@
 #!/usr/bin/env python
 
-from netaddr import EUI, mac_unix
-from sys import argv, exit
-import csv, re, urllib2, json
+from argparse import ArgumentParser
+from os import path
+import csv, re, urllib2, json, netaddr, sys
 
 
 class MacLookup(object):
-    def __init__(self, csv_path):
-        self.csv_path = csv_path
+    def __init__(self, csv_src, csv_dst):
+        self.csv_src = csv_src
+        self.csv_dst = csv_dst
+        self.csv_fields = ["MAC", "Brand", "Prefix"]
         self.vendor_details = {}
         self.r = re.compile(r'(?:[0-9a-fA-F]:?){12}')
         self.get_mac()
 
     def get_mac(self):
-        try:
-            with open(self.csv_path, 'rb') as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    mac = row[0]
-                    if self.r.match(mac):
-                        #if mac in self.vendor_details.keys():
-                        #    vendor_detail = self.get_vendor_details(mac)
-                        #    mac_dialect = vendor_detail[0]
-                        #    company = vendor_detail[1]
-                        #    mac_prefix = vendor_detail[2]
-                        #    self.vendor_details[mac].extend([(mac_dialect, company, mac_prefix)])
-                        #else:
-                        vendor_detail = self.get_vendor_details(mac)
-                        mac_dialect = vendor_detail[0]
-                        company = vendor_detail[1]
-                        mac_prefix = vendor_detail[2]
-                        self.vendor_details[mac]=[(mac_dialect, company, mac_prefix)]
-                    else:
-                        mac = row[0]
-                        self.vendor_details[mac]='Invalid MAC'
-            f.close()
-            self.get_output()
-        except IOError as e:
-            print("Error %s, %s" % (e.errno, e.strerror))
-            exit(1)
+        macs = self.csv_reader()
+        for mac, state in macs.items():
+            if self.r.match(mac):
+                vendor_detail = self.get_vendor_details(mac)
+                mac_dialect = vendor_detail[0]
+                company = vendor_detail[1]
+                mac_prefix = vendor_detail[2]
+                try:
+                    self.vendor_details[mac_dialect]=(company, mac_prefix,
+                                                      state)
+                except KeyError as e:
+                    print("%s triggered a key error" % (e))
+                    sys.exit(1)
+            elif mac == self.csv_fields[0]:
+                continue
+            else:
+                print("%s is not a valid MAC" % (mac))
+        #self.get_output()
+        self.csv_writer()
 
+    def csv_reader(self):
+        macs = {}
+        for source in self.csv_src:
+            try:
+                date = path.splitext(path.basename(source))[0]
+                self.csv_fields.append(date)
+                with open(source, "rb") as f:
+                    reader = csv.reader(f)
+                    for row in reader:
+                        mac = row[0]
+                        state = row[1].upper()
+                        macs[mac]=[date, state]
+            except IOError as e:
+                print("Error code %d while reading %s: %s" %
+                      (e.errno, source, e.strerror))
+                sys.exit(1)
+        return(macs)
+
+    def csv_writer(self):
+        try:
+            with open(self.csv_dst, "wb") as f:
+                writer = csv.DictWriter(f, fieldnames=self.csv_fields)
+                writer.writeheader()
+                for key, value in self.vendor_details.items():
+                    writer.writerow({"MAC": key, "Brand": value[0],
+                                    "Prefix": value[1],
+                                    value[2][0]: value[2][1]})
+        except IOError as e:
+            print("Error code %d while writing %s: %s" %
+                  (e.errno, self.csv_dst, e.strerror))
+            sys.exit(1)
+
+    def csv_add(self):
+        print 'hoi'
+        
     def get_vendor_details(self, mac):
-        converted_mac = EUI(mac)
-        converted_mac.dialect = mac_unix
+        converted_mac = netaddr.EUI(mac)
+        converted_mac.dialect = netaddr.mac_unix
         try:
             oui = converted_mac.oui
         except:
             company, mac_prefix = self.get_vendor_details_online(converted_mac)
-            mac_prefix = mac_prefix.replace(':', '-')
+            mac_prefix = mac_prefix.replace(":", "-")
             return(str(converted_mac), str(company), str(mac_prefix))
-        return(str(converted_mac), str(oui.registration().org), str(oui.registration().oui))
+        return(str(converted_mac),
+               str(oui.registration().org),
+               str(oui.registration().oui))
 
     def get_vendor_details_online(self, mac):
         opener = urllib2.build_opener()
-        opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+        opener.addheaders = [("User-Agent", "Mozilla/5.0")]
         response = opener.open("https://macvendors.co/api/%s/json" % (mac))
         vendor_detail = json.loads(response.read())
-        if 'result' in vendor_detail.keys():
+        if "result" in vendor_detail.keys():
             try:
-                company = vendor_detail['result']['company']
-                mac_prefix = vendor_detail['result']['mac_prefix']
+                company = vendor_detail["result"]["company"]
+                mac_prefix = vendor_detail["result"]["mac_prefix"]
                 return(company, mac_prefix)
             except KeyError:
-                return('N/A', 'N/A')
+                return("N/A", "N/A")
+        else:
+            print("was not found")
 
     def get_output(self):
         for key, value in self.vendor_details.items():
-            print("%s: %s" % (key, value))
+            print("Key: %s\nCompany: %s\nPrefix: %s\nDate: %s\nState: %s\n\n" %
+                  (key, value[0], value[1], value[2][0], value[2][1]))
 
-    def write_csv(self):
-        print('write')
+
+def argument_parser():
+    parser = ArgumentParser(description="Mac address details -> csv")
+    parser.add_argument("-s", "--src", action="append", type=str, required=True,
+                        help="Provide source csv")
+    parser.add_argument("-d", "--dst", type=str, required=True,
+                        help="Provide destination csv")
+    args = vars(parser.parse_args())
+    return(args)
 
 
 def main():
-    MacLookup('/home/k.dejong/Downloads/scan_results_10-2-2017.csv')
+    args = argument_parser()
+    sources = args["src"]
+    destination = args["dst"]
+    MacLookup(sources, destination)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
