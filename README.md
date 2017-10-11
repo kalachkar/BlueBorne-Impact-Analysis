dcc1d4daacd9cde5a97ac80b27fd9c1aebc3ca18
Kees de Jong <keesdejong+bugs@gmail.com>
2017-10-11 22:05:49 +0200

    Squashed commit of the following:
    commit 397b64c8bc6bc291f3ebe51172f8589ee1b92430
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 11 22:04:41 2017 +0200
    
        scanner is useless, remove
    
    commit 34e177bab3e348513d47e27ffac6a95370eb90a9
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 11 22:04:02 2017 +0200
    
        added exit code
    
    commit ab9a16fb8aa14c6ba9ba46f76e30ba45eb684a72
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 11 16:41:07 2017 +0200
    
        added more meaningful error output for get_vendor_details_online method
    
    commit 4b5bb971c0f35e18edde853cca38b88c32df7bb0
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 11 16:28:35 2017 +0200
    
        removed the last attempt to be pep8 compliant, serves no purpose
    
    commit b880693ca3597183fafb1022537d21098f6631a4
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 11 16:26:14 2017 +0200
    
        removed legacy solution
    
    commit 5a3eebe9943bce24bccb3bb547f2220871881577
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 11 16:15:18 2017 +0200
    
        updated the readme with command line example
    
    commit 9c3f6cd2e00e19e71728a8a95692df6870b504f7
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 11 16:03:27 2017 +0200
    
        working version, testing needed
    
    commit ea31e6a20c32127d3f96811173ac7a0054c2eeb5
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Tue Oct 10 15:54:53 2017 +0200
    
        FIXME: update columns without replacing the whole row
    
    commit da8cd3572bf7063a1e17208368f4d6e23b8f63e9
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Fri Oct 6 10:12:51 2017 +0200
    
        time to add method to write new csv
    
    commit 500b20f68c414a733780fb426eb4ec3a45a1d018
    Author: Kees de Jong <keesdejong+bugs@gmail.com>
    Date:   Wed Oct 4 09:19:53 2017 +0200
    
        initial beta version, online mac info check needed + cvs updater

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SSN project
-This repo is to manage source code for our SSN project (SNE master)
+
+## Introduction
+This repo is to manage source code for the SSN project (SNE master).
+
+## Features
+ * MAC lookup (both online and offline via the EUI info in the csv Python module)
+ * Merge and process data from multiple csv files (with a MAC address column and a column with the BlueBorne vulnerability state, the file name of the csv should be the date of the scan)
+
+## Examples
+```bash
+./mac_lookup.py -s 10-01-2017.csv -s 10-02-2017.csv -d /tmp/final.csv
+```
---
+++ mac_lookup.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from os import path
+import pandas as pd
+import csv, re, urllib2, json, netaddr, sys
+
+
+class MacLookup(object):
+    def __init__(self, csv_src, csv_dst):
+        self.csv_src = csv_src
+        self.csv_dst = csv_dst
+        self.csv_fields = ["MAC", "Brand", "Prefix"]
+        self.vendor_details = {}
+        self.state_info = {}
+        self.r = re.compile(r'(?:[0-9a-fA-F]:?){12}')
+        self.get_mac()
+
+    def get_mac(self):
+        macs = self.csv_reader()
+        for mac, state in macs.items():
+            if self.r.match(mac):
+                vendor_detail = self.get_vendor_details(mac)
+                mac_dialect = vendor_detail[0]
+                company = vendor_detail[1]
+                mac_prefix = vendor_detail[2]
+                try:
+                    self.vendor_details[mac_dialect]=(company, mac_prefix, state)
+                except KeyError as e:
+                    print("%s triggered a key error" % (e))
+                    sys.exit(1)
+            elif mac == self.csv_fields[0]:
+                continue
+            else:
+                print("%s is not a valid MAC" % (mac))
+        self.state_info = {key: value[2] for key, value in self.vendor_details.items()}
+        self.csv_writer()
+
+    def csv_reader(self):
+        macs = {}
+        for source in self.csv_src:
+            try:
+                date = path.splitext(path.basename(source))[0]
+                self.csv_fields.append(date)
+                with open(source, "rb") as f:
+                    reader = csv.reader(f)
+                    for row in reader:
+                        mac = row[0]
+                        state = row[1].upper()
+                        if not mac in macs.keys():
+                            macs[mac]=["%s|%s" % (date, state)]
+                        else:
+                            macs[mac].append("%s|%s" % (date, state))
+            except IOError as e:
+                print("Error code %d while reading %s: %s" % (e.errno, source, e.strerror))
+                sys.exit(1)
+        return(macs)
+
+    def csv_writer(self):
+        try:
+            with open(self.csv_dst, "wb") as f:
+                writer = csv.DictWriter(f, fieldnames=self.csv_fields)
+                writer.writeheader()
+                for key, value in self.vendor_details.items():
+                    mac = key
+                    brand = value[0]
+                    prefix = value[1]
+                    writer.writerow({"MAC": mac, "Brand": brand, "Prefix": prefix})
+        except IOError as e:
+            print("Error code %d while writing %s: %s" % (e.errno, self.csv_dst, e.strerror))
+            sys.exit(1)
+        self.csv_update_columns()
+
+    def csv_update_columns(self):
+        df = pd.read_csv(self.csv_dst)
+        for key, value in self.state_info.items():
+            for x in value:
+                x = x.split("|")
+                mac = key
+                date = x[0]
+                state = x[1]
+                df.loc[df[self.csv_fields[0]] == mac, date] = state
+        df.to_csv(self.csv_dst, sep=",", encoding="utf-8")
+
+    def get_vendor_details(self, mac):
+        converted_mac = netaddr.EUI(mac)
+        converted_mac.dialect = netaddr.mac_unix
+        try:
+            oui = converted_mac.oui
+        except:
+            company, mac_prefix = self.get_vendor_details_online(converted_mac)
+            mac_prefix = mac_prefix.replace(":", "-")
+            return(str(converted_mac), str(company), str(mac_prefix))
+        return(str(converted_mac), str(oui.registration().org), str(oui.registration().oui))
+
+    def get_vendor_details_online(self, mac):
+        opener = urllib2.build_opener()
+        opener.addheaders = [("User-Agent", "Mozilla/5.0")]
+        response = opener.open("https://macvendors.co/api/%s/json" % (mac))
+        vendor_detail = json.loads(response.read())
+        if "result" in vendor_detail.keys():
+            try:
+                company = vendor_detail["result"]["company"]
+                mac_prefix = vendor_detail["result"]["mac_prefix"]
+                return(company, mac_prefix)
+            except KeyError:
+                return("N/A", "N/A")
+        else:
+            print("Something went wrong in get_vendor_details_online, go to red alert")
+            sys.exit(1)
+
+
+def argument_parser():
+    parser = ArgumentParser(description="Mac address details -> csv")
+    parser.add_argument("-s", "--src", action="append", type=str, required=True, help="Provide source csv")
+    parser.add_argument("-d", "--dst", type=str, required=True, help="Provide destination csv")
+    args = vars(parser.parse_args())
+    return(args)
+
+
+def main():
+    args = argument_parser()
+    sources = args["src"]
+    destination = args["dst"]
+    MacLookup(sources, destination)
+
+
+if __name__ == "__main__":
+    main()
